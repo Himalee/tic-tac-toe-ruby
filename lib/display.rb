f2f8f292e80c7e31b30f6cb248d0462c19e53b0d
Himalee <himalee.tailor@gmail.com>
2018-08-15 17:51:38 +0100

    Refactor: present_board()

--- lib/display.rb
+++ lib/display.rb
@@ -5,6 +5,10 @@ class Display
   end
 
   def present_board(grid)
-    @console.present(" #{grid[0]} | #{grid[1]} | #{grid[2]} \n===+===+===\n #{grid[3]} | #{grid[4]} | #{grid[5]} \n===+===+===\n #{grid[6]} | #{grid[7]} | #{grid[8]} \n")
+    @console.present(" #{grid[0]} | #{grid[1]} | #{grid[2]} #{new_line} #{grid[3]} | #{grid[4]} | #{grid[5]} #{new_line} #{grid[6]} | #{grid[7]} | #{grid[8]} \n")
+  end
+
+  def new_line
+    "\n===+===+===\n"
   end
 end
