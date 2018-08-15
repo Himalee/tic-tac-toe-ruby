734f7a6e31b54ae8735466a5dc45136da5d77aed
Himalee <himalee.tailor@gmail.com>
2018-08-15 18:13:39 +0100

    Fix: rename variables and implement private methods

--- lib/game.rb
+++ lib/game.rb
@@ -10,7 +10,7 @@ class Game
     display_board
     @display.prompt_for_cell
     until @board.end_of_game?(@board.grid)
-      get_human_spot
+      get_human_cell
       if !@board.end_of_game?(@board.grid)
         eval_board
       end
@@ -19,47 +19,49 @@ class Game
     @display.game_over
   end
 
+  private
+
   def display_board
     @display.present_board(@board.grid)
   end
 
-  def get_human_spot
-    spot = nil
-    until spot
-      spot = @display.get_cell
-      @board.mark_grid_with_valid_cell(spot, @hum)
+  def get_human_cell
+    cell = nil
+    until cell
+      cell = @display.get_cell
+      @board.mark_grid_with_valid_cell(cell, @hum)
     end
   end
 
   def eval_board
-    spot = nil
-    until spot
+    cell = nil
+    until cell
       if @board.grid[4] == "4"
-        spot = 4
-        @board.mark_grid(@board.grid, spot, @com)
+        cell = 4
+        @board.mark_grid(@board.grid, cell, @com)
       else
-        spot = get_best_move(@board.grid, @com)
-        @board.mark_grid_with_valid_cell(spot, @com)
+        cell = get_best_move(@board.grid, @com)
+        @board.mark_grid_with_valid_cell(cell, @com)
       end
     end
   end
 
   def get_best_move(board, next_player, depth = 0, best_score = {})
     best_move = nil
-    @board.available_spaces(board).each do |as|
-      board[as.to_i] = @com
+    @board.available_spaces(board).each do |cell|
+      board[cell.to_i] = @com
       if @board.win?(board)
-        best_move = as.to_i
-        board[as.to_i] = as
+        best_move = cell.to_i
+        board[cell.to_i] = cell
         return best_move
       else
-        board[as.to_i] = @hum
+        board[cell.to_i] = @hum
         if @board.win?(board)
-          best_move = as.to_i
-          board[as.to_i] = as
+          best_move = cell.to_i
+          board[cell.to_i] = cell
           return best_move
         else
-          board[as.to_i] = as
+          board[cell.to_i] = cell
         end
       end
     end
