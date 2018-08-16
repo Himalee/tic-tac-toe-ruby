46e3f39bafbd942a588d924c6c50ecc3ef6bc140
Himalee <himalee.tailor@gmail.com>
2018-08-16 14:56:26 +0100

    Add: user cell input validation

--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -9,5 +9,12 @@ describe Validator do
       validator = Validator.new(board)
       expect(validator.valid_cell?(1)).to be true
     end
+
+    it "returns false given invalid choice" do
+      grid = ["X", 1, 2, 3, 4, 5, 6, 7, 8]
+      board = Board.new(grid)
+      validator = Validator.new(board)
+      expect(validator.valid_cell?(0)).to be false
+    end
   end
 end
