7f7090123ea4c163c58be23ac350ce5c2f035ded
Himalee <himalee.tailor@gmail.com>
2018-08-16 15:41:07 +0100

    Test: add invalid input to integration test

--- spec/integration/game_spec.rb
+++ spec/integration/game_spec.rb
@@ -5,7 +5,7 @@ describe Game do
   before :each do
     grid = ["O", "X", "O", "X", "O", "X", "X", 7, 8]
     @board = Board.new(grid)
-    input = StringIO.new("7")
+    input = StringIO.new("0\nhello\n12\n7")
     @output = StringIO.new
     console = Console.new(@output, input)
     validator = Validator.new
