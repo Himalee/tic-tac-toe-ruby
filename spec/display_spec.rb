9a65544c62bec21055c549ec852c911f95c1c7df
Himalee <himalee.tailor@gmail.com>
2018-08-16 15:13:36 +0100

    Refactor: inject Validator into Display

--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -5,9 +5,11 @@ describe Display do
   it "presents board" do
     output = StringIO.new
     console = Console.new(output)
-    display = Display.new(console)
     grid = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    display.present_board(grid)
+    board = Board.new(grid)
+    validator = Validator.new(board)
+    display = Display.new(console, validator)
+    display.present_board(board.grid)
     expect(output.string).to eql(" 0 | 1 | 2 \n===+===+===\n 3 | 4 | 5 \n===+===+===\n 6 | 7 | 8 \n")
   end
 end
