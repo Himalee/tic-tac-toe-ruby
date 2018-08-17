414b2cf2d4630c28ff4763d53619a79cf8cffda4
Himalee <himalee.tailor@gmail.com>
2018-08-17 13:27:22 +0100

    Test: non integer user input validation

--- spec/human_player_spec.rb
+++ spec/human_player_spec.rb
@@ -7,15 +7,26 @@ require "message"
 
 describe HumanPlayer do
 
-  it "validate user input" do
+  before :each do
     grid = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    board = Board.new(grid)
-    validator = Validator.new
-    output = StringIO.new
+    @board = Board.new(grid)
+    @validator = Validator.new
+    @output = StringIO.new
+  end
+
+  it "validate user input with integer input" do
     input = StringIO.new("33\n3")
-    console = Console.new(output, input)
-    display = Display.new(console, validator)
+    console = Console.new(@output, input)
+    display = Display.new(console, @validator)
     human_player = HumanPlayer.new(display)
-    expect(human_player.get_move(board)).to eql(3)
+    expect(human_player.get_move(@board)).to eql(3)
+  end
+
+  it "validate user input with non integer input" do
+    input = StringIO.new("hello\n45\n4")
+    console = Console.new(@output, input)
+    display = Display.new(console, @validator)
+    human_player = HumanPlayer.new(display)
+    expect(human_player.get_move(@board)).to eql(4)
   end
 end
