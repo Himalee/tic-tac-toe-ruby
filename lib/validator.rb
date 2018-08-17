ed1e4c5512835db7c19ceff08d4122771d78d503
Himalee <himalee.tailor@gmail.com>
2018-08-17 15:30:19 +0100

    Fix: non integer user input validation

--- lib/validator.rb
+++ lib/validator.rb
@@ -3,4 +3,8 @@ class Validator
   def valid_cell?(board, choice)
     board.available_spaces(board.grid).include?(choice)
   end
+
+  def valid_input?(input)
+    ("0".."8").to_a.include?(input)
+  end
 end
