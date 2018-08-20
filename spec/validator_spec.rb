f0ca963689b808b14c141330ac8533a915651ca7
Himalee <himalee.tailor@gmail.com>
2018-08-20 13:46:16 +0100

    Fix: remove duplication from tests

--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -3,18 +3,21 @@ require "validator"
 describe Validator do
 
   context "validate cell choice" do
+
+    before :each do
+      @validator = Validator.new
+    end
+
     it "returns true given valid choice" do
       grid = [0, 1, 2, 3, 4, 5, 6, 7, 8]
       board = Board.new(grid)
-      validator = Validator.new
-      expect(validator.valid_cell?(board, 1)).to be true
+      expect(@validator.valid_cell?(board, 1)).to be true
     end
 
     it "returns false given invalid choice" do
       grid = ["X", 1, 2, 3, 4, 5, 6, 7, 8]
       board = Board.new(grid)
-      validator = Validator.new
-      expect(validator.valid_cell?(board, 0)).to be false
+      expect(@validator.valid_cell?(board, 0)).to be false
     end
   end
 end
