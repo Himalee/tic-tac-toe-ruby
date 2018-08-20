4b31a67593f95f4a9fbb5edf5775b539d54c1c3c
Himalee <himalee.tailor@gmail.com>
2018-08-20 10:50:56 +0100

    Refactor: remove duplication from Game

--- lib/game.rb
+++ lib/game.rb
@@ -10,35 +10,32 @@ class Game
   def start_game
     @display.welcome
     display_board
-    until @board.end_of_game?(@board.grid)
-      @display.prompt_for_cell
-      get_human_cell
-      if !@board.end_of_game?(@board.grid)
-        get_computer_cell
-      end
-    end
+    player_turns
     @display.game_over
   end
 
   private
 
-  def get_human_cell
-    cell = @players[0].get_move(@board)
-    player_turn(cell, @players[0].mark)
-  end
-
-  def get_computer_cell
-    cell = @players[1].get_move(@board)
-    player_turn(cell, @players[1].mark)
-  end
-
-  def player_turn(cell, mark)
-    @board = @board.new_board(cell, mark)
-    display_board
-    @display.chosen_cell(cell)
-  end
-
   def display_board
     @display.present_board(@board.grid)
+  end
+
+  def player_turns
+    until @board.end_of_game?(@board.grid)
+      @display.prompt_for_cell
+      cell = current_player.get_move(@board)
+      @board = @board.new_board(cell, current_player.mark)
+      display_board
+      @display.chosen_cell(cell)
+      next_player
+    end
+  end
+
+  def current_player
+    @players[0]
+  end
+
+  def next_player
+    @players.rotate!
   end
 end
