0ca8a3d1e1adc3925d8936acb0eef6beec66ca59
Himalee <himalee.tailor@gmail.com>
2018-09-11 17:34:38 +0100

    Refactor: clean up tests in game_factory_spec

--- spec/integration/game_factory_spec.rb
+++ spec/integration/game_factory_spec.rb
@@ -1,21 +1,15 @@
 require "game_factory"
 
 describe GameFactory do
-
-  context "creates a Game" do
-    before :each do
-      output = StringIO.new
-      input = StringIO.new("1\nO\nX\n2")
-      console = Console.new(output, input)
-      validator = Validator.new
-      display = Display.new(console, validator)
-      player_factory = PlayerFactory.new(display)
-      game_mode = GameMode.new
-      @game_factory = GameFactory.new(player_factory, game_mode, display)
-    end
-
-    it "returns a Game" do
-      expect(@game_factory.create_game).to be_a(Game)
-    end
+  it "returns a Game" do
+    output = StringIO.new
+    input = StringIO.new("1\nO\nX\n2")
+    console = Console.new(output, input)
+    validator = Validator.new
+    display = Display.new(console, validator)
+    player_factory = PlayerFactory.new(display)
+    game_mode = GameMode.new
+    game_factory = GameFactory.new(player_factory, game_mode, display)
+    expect(game_factory.create_game).to be_a(Game)
   end
 end
